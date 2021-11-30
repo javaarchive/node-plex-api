22f5d27c5e5f139ffd7841fce6d3a81728fd4b71
Raymomyar <awesomecatstudio@gmail.com>
2021-11-29 17:56:23 -0800

    api credentials still needs request and it's too much effort for now to deal with it

---
+++ quicktest.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const PlexAPI = require('./lib/api');
+
+const plexClient = new PlexAPI({
+    // localhost autoconnect
+    username: process.env.PLEX_USERNAME,
+    password: process.env.PLEX_PASSWORD
+});
+
+plexClient.query("/").then(function (result) {
+	console.log("%s running Plex Media Server v%s",
+		result.friendlyName,
+		result.version);
+
+	// array of children, such as Directory or Server items
+	// will have the .uri-property attached
+	console.log(result._children);
+}, function (err) {
+	console.error("Could not connect to server", err);
+});
